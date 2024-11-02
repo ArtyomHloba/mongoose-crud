1ee5b0fe57928df3006c3e52e3c10eb977307531
ArtyomHloba <artemgloba2@gmail.com>
2024-11-02 19:28:14 +0200

    Add controllers: create, delete, getAll, getById, update

--- models/user.js
+++ models/user.js
@@ -8,49 +8,52 @@ const EMAIL_VALIDATION_SCHEMAS = yup.string().email();
 // Numbers, Date: min, max
 // Strings: enum, match, minLength, maxLength
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    minLength: 2,
-    maxLength: 50,
-    required: true,
-    match: [
-      /^[A-Z][a-z]+$/,
-      'FirstName must starts  with capital letter, but got a "{VALUE}" ...',
-    ],
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      minLength: 2,
+      maxLength: 50,
+      required: true,
+      match: [
+        /^[A-Z][a-z]+$/,
+        'FirstName must starts  with capital letter, but got a "{VALUE}" ...',
+      ],
+    },
+    lastName: {
+      type: String,
+      minLength: 2,
+      maxLength: 50,
+      required: true,
+      match: /^[A-Z][a-z]+$/,
+    },
+    email: {
+      type: String,
+      // match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      validate: v => EMAIL_VALIDATION_SCHEMAS.isValid(v),
+      required: true,
+    },
+    birthday: {
+      type: Date,
+      max: new Date(),
+    },
+    gender: {
+      type: String,
+      // TODO move to constants
+      enum: ['male', 'female', 'other'],
+    },
+    isMarried: {
+      type: Boolean,
+      default: false,
+    },
+    workExperience: {
+      type: Number,
+      min: 0,
+      max: 100,
+    },
   },
-  lastName: {
-    type: String,
-    minLength: 2,
-    maxLength: 50,
-    required: true,
-    match: /^[A-Z][a-z]+$/,
-  },
-  email: {
-    type: String,
-    // match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-    validate: v => EMAIL_VALIDATION_SCHEMAS.isValid(v),
-    required: true,
-  },
-  birthday: {
-    type: Date,
-    max: new Date(),
-  },
-  gender: {
-    type: String,
-    // TODO move to constants
-    enum: ['male', 'female', 'other'],
-  },
-  isMarried: {
-    type: Boolean,
-    default: false,
-  },
-  workExperience: {
-    type: Number,
-    min: 0,
-    max: 100,
-  },
-});
+  { versionKey: false }
+);
 
 const User = mongoose.model('User', userSchema);
 
